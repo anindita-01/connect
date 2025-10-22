42f42ab73d6d62970a74a9cab5fb45d3c6143d16
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-10-22 17:52:47 +0530

    add: authentication

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -25,6 +25,18 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    nativeLanguage: {
+      type: String,
+      default: "",
+    },
+    learningLanguage: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
     isOnboarded: {
       type: Boolean,
       default: false,
@@ -35,29 +47,30 @@ const userSchema = new mongoose.Schema(
         ref: "User",
       },
     ],
-    //native lang, learning lang, location- 31:10
   },
   { timestamps: true }
 );
 
-const User= mongoose.model("User",userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-//pre hook(hash passwords)
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-userSchema.pre("save", async(next)=>{
-    if(!this.isModified("password")) return next(); //if password is not modified do not try to hash it
-    try{
-        const salt= await bcrypt.genSalt(10);
-            this.password=await bcrypt.hash(this.password, salt);
-            next();
-        
-    }catch(error){
-        next(error);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  const isPasswordCorrect = await bcrypt.compare(
+    enteredPassword,
+    this.password
+  );
+  return isPasswordCorrect;
+};
 
-
-    }
-
-
-})
+const User = mongoose.model("User", userSchema);
 
 export default User;
