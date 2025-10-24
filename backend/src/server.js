3655af8b41bf6b11fdd4d04aaeb2118a0d82304a
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-10-24 12:11:34 +0530

    updatte: env

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true, // allow frontend to send cookies
   })
 );
